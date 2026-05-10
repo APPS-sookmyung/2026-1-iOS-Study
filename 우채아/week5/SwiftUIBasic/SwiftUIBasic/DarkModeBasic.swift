a7fd2e9dd0474ce5bf501f786faaba5134a7c49a
chaeeah <dncodk@naver.com>
2026-05-10 23:55:50 +0900

    Feat: 5주차 실습 코드 및 정리

---
+++ 우채아/week5/SwiftUIBasic/SwiftUIBasic/DarkModeBasic.swift
@@ -0,0 +1,44 @@
+//
+//  DarkModeBasic.swift
+//  SwiftUIBasic
+//
+//  Created by 우채아 on 5/10/26.
+//
+
+import SwiftUI
+
+struct DarkModeBasic: View {
+    // property
+    @Environment(\.colorScheme) var colorScheme
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                VStack (spacing: 20) {
+                    // primary and secondary color 는 자동으로 다크 모드일때 색 변환 됨
+                    Text("Primary 색상")
+                        .foregroundColor(.primary)
+                    Text("Secondary 색상")
+                        .foregroundColor(.secondary)
+                    // 일반적인 black , white 는 고정값임
+                    Text("Black Color")
+                        .foregroundColor(.black)
+                    Text("White Color")
+                        .foregroundColor(.white)
+                    Text("Red Color")
+                        .foregroundColor(.red)
+                    // Asset 에서 adaptive 라는 이름의 color Set 을 만든 후 light, dark mode 에서 색을 설정해줄 수 있음
+                    Text("Asset 에서 Adpative 색 설정")
+                        .foregroundColor(Color("AdaptiveColor"))
+                    // @Environment 사용해서 colorSchme 사용하기
+                    Text("@Environment 사용해서 Adpative 색 설정")
+                        .foregroundColor(colorScheme == .light ? .green : .blue)
+                }
+            }//: SCROLL
+        } //:NAVIGATION
+    }
+}
+
+#Preview {
+    DarkModeBasic()
+}
